2e4e0498749648fa7b533b0d7008679c8ec3ea34
niisan-tokyo <niikura23@gmail.com>
2020-05-13 00:33:12 +0900

    implement crawler and image links template

--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/antchfx/xmlquery v1.2.4 // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/gocolly/colly v1.2.0 // indirect
+	github.com/gocolly/colly v1.2.0
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/rakyll/statik v0.1.7
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gocolly/colly"
 	"github.com/rakyll/statik/fs"
 
 	//"fmt"
@@ -13,6 +14,10 @@ import (
 
 	_ "github.com/niisan-tokyo/image-crawler/statik"
 )
+
+type imageLinks struct {
+	Links []string
+}
 
 func main() {
 	r := gin.Default()
@@ -31,6 +36,24 @@ func main() {
 
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "/form.tmpl", gin.H{})
+	})
+
+	// スクレイピングしたものを表示
+	r.GET("scrape", func(c *gin.Context) {
+		url := c.Query("url")
+		p := &imageLinks{Links: []string{}}
+
+		coll := colly.NewCollector()
+		coll.OnHTML("img", func(e *colly.HTMLElement) {
+			src := e.Request.AbsoluteURL(getImgSrc(e))
+			if src != "" {
+				p.Links = append(p.Links, src)
+			}
+		})
+		coll.Visit(url)
+		c.HTML(http.StatusOK, "/scrape.tmpl", gin.H{
+			"links": p.Links,
+		})
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
@@ -71,4 +94,18 @@ func loadTemplate() (*template.Template, error) {
 	})
 
 	return t, err
+}
+
+func getImgSrc(e *colly.HTMLElement) string {
+	src := e.Attr("data-lazy-src")
+	if src != "" {
+		return src
+	}
+
+	src2 := e.Attr("data-wpfc-original-src")
+	if src2 != "" {
+		return src2
+	}
+
+	return e.Attr("src")
 }
