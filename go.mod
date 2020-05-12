f47a905c06217cef260dce2541686afe1cbceb36
niisan-tokyo <niikura23@gmail.com>
2020-05-12 23:46:37 +0900

    create page input URL

--- go.mod
+++ go.mod
@@ -6,10 +6,11 @@ require (
 	github.com/PuerkitoBio/goquery v1.5.1 // indirect
 	github.com/antchfx/htmlquery v1.2.3 // indirect
 	github.com/antchfx/xmlquery v1.2.4 // indirect
-	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/gin-gonic/gin v1.6.3
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly v1.2.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/rakyll/statik v0.1.7
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
 	golang.org/x/net v0.0.0-20200506145744-7e3656a0809f // indirect
--- main.go
+++ main.go
@@ -1,17 +1,74 @@
 package main
 
 import (
+	"html/template"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/rakyll/statik/fs"
+
+	//"fmt"
+	"io/ioutil"
+	"os"
+
+	_ "github.com/niisan-tokyo/image-crawler/statik"
 )
 
 func main() {
 	r := gin.Default()
-	//r.LoadHTMLGlob("templates/*")
+
+	t, err := loadTemplate()
+	if err != nil {
+		panic(err)
+	}
+	r.SetHTMLTemplate(t)
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "/form.tmpl", gin.H{})
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func loadTemplate() (*template.Template, error) {
+	t := template.New("")
+	statikFS, err := fs.New()
+	if err != nil {
+		return nil, err
+	}
+
+	// 仮想ファイルシステム上を走査する
+	err = fs.Walk(statikFS, "/", func(path string, info os.FileInfo, err error) error {
+		// ディレクトリはスキップ
+		if info.IsDir() {
+			return nil
+		}
+		r, err := statikFS.Open(path)
+		if err != nil {
+			return err
+		}
+		defer r.Close()
+
+		// データ読み出し
+		h, err := ioutil.ReadAll(r)
+		if err != nil {
+			return err
+		}
+
+		// テンプレートにpath名で読み出したデータをパースして格納
+		t, err = t.New(path).Parse(string(h))
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	return t, err
 }
